6904f801b5190bdc7b381fa02e2af8e966bcb35e
bilginmrv <47225405+mervebilgin@users.noreply.github.com>
2022-05-28 19:25:09 +0300

    CRUD operations

--- test.js
+++ test.js
@@ -2,11 +2,6 @@ const mongoose = require('mongoose')
 
 const Post = require('./models/Post')
 
-/*mongoose.connect('mongodb://127.0.0.1/nodeblog_test_db', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})*/
-
 mongoose.connect('mongodb://127.0.0.1/nodeblog_test_db', {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -17,39 +12,3 @@ mongoose.connect('mongodb://127.0.0.1/nodeblog_test_db', {
 Post.findByIdAndDelete('6110e392d21b0b58c4cb860a', (error, post) => {
   console.log(error, post)
 })
-
-/*Post.findById('6110e237920315547063b5a2', (error, post) => {
-  console.log(error, post)
-})*/
-
-//Yukarıdaki yorum satırındaki postu update edebilmek için
-
-/* Post.findByIdAndUpdate('6110e237920315547063b5a2', {
-  title: 'Benim 1. Postum'
-}, (error, post) => {
-  console.log(error, post)
-}) */
-
-
-
-/* Post.find({ }, (error, post) => {
-  console.log(error, post)
-})  *//*veritabanı ile bağlantı kurulan tüm verileri bize sunar*/ 
-
-/*
-Ctrl+c ile kodu tekrarrdan başlatabiliriz. 
-Post.find({
-  title: 'İkinci post başlığım'
-}, (error, post) => {
-  console.log(error, post)
-})*/
-/*Veritabanından title'ı 'İkinci post başlığım' olan datayı çeker*/
-
-/*
-Post.create({
-    title: 'İkinci post başlığım', 
-    content: 'İkinci post içeriği, lorem ipsum text'
-}, (error, post) => { //callback fonksiyonu
-    console.log(error, post)
-})*/
-
