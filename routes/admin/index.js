0fc1e5939af82c4b80fc890d037e6497155bfb4f
bilginmrv <47225405+mervebilgin@users.noreply.github.com>
2022-05-29 00:34:57 +0300

    Post admin area, post - category relationship

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Category = require('../../models/Category')
+const Post = require('../../models/Post')
 
 
 router.get('/', (req, res) => {
@@ -29,6 +30,12 @@ router.delete('/categories/:id', (req, res) => {
     Category.remove({_id : req.params.id}).then(()=>{
         res.redirect('/admin/categories')
     })
+})
+
+router.get('/posts', (req, res) => {
+    Post.find({}).populate({path: 'category', model: Category}).sort({$natural:-1}).lean().then(posts => {
+        res.render('admin/posts', {posts:posts})
+    }) 
 })
 
 module.exports = router
